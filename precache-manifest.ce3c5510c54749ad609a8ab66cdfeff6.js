adb325612a3508b1e78e97a7cd67132c18b432aa
Jessica May <mayjessican@gmail.com>
2020-09-24 16:17:12 +0200

    Updates

---
+++ precache-manifest.ce3c5510c54749ad609a8ab66cdfeff6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf158dcd4f886eb9f696b382fc1f137",
+    "url": "/meet/index.html"
+  },
+  {
+    "revision": "648681bd2b80e2e2bbdd",
+    "url": "/meet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47648c616e649b715f42",
+    "url": "/meet/static/js/2.63d82722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meet/static/js/2.63d82722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "648681bd2b80e2e2bbdd",
+    "url": "/meet/static/js/main.88bcf5b3.chunk.js"
+  },
+  {
+    "revision": "9de4f142a6170bcd9b6d",
+    "url": "/meet/static/js/runtime-main.496a315d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet/static/media/logo.5d5d9eef.svg"
+  }
+]);
