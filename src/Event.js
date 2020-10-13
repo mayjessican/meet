676ef2ae7df9974b4fe885d12133502f1b3f8405
mayjessican <63651624+mayjessican@users.noreply.github.com>
2020-10-13 12:12:04 +0200

    updated test to better reflect mockdata

--- src/Event.js
+++ src/Event.js
@@ -11,38 +11,17 @@ class Event extends Component {
     }));
   };
 
-  // handleShowDetails = () => {
-  //   if (this.state.showDetails === false) {
-  //     this.setState({ showDetails: true });
-  //   } else {
-  //     this.setState({ showDetails: false });
-  //   }
-  // };
   render() {
     const event = this.props.event;
     return (
       <div className="Event">
         <p className="time">
-          {event.time} - {event.date}
+          {event.start.dateTime} - {event.end.dateTime}
         </p>
         <p className="name">{event.name}</p>
-        {event.group && event.group.name && (
-          <p className="group-name">Group: {event.group.name}</p>
-        )}
-        <p className="going">{event.yes_rsvp_count} people who are going</p>
         {this.state.showDetails && (
           <div className="extra">
-            {event.venue && event.venue.name && (
-              <p className="address">
-                {event.venue.name +
-                  ", " +
-                  event.venue.address_1 +
-                  ", " +
-                  event.venue.city +
-                  ", " +
-                  event.venue.localized_country_name}
-              </p>
-            )}
+            <p className="location">{event.location}</p>
             <div
               className="description"
               dangerouslySetInnerHTML={{ __html: event.description }}
--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -13,21 +13,19 @@ describe("<Event /> component", () => {
     EventWrapper = shallow(
       <Event
         event={{
-          date: "2020-05-19",
-          time: "16:00",
+          start: {
+            dateTime: "2020-05-19T16:00:00+02:00",
+          },
+          end: {
+            dateTime: "2020-05-19T17:00:00+02:00",
+          },
           name: "Learn JavaScript",
-          yes_rsvp_count: 90,
           link:
             "https://www.google.com/calendar/event?eid=NGVhaHM5Z2hraHJ2a2xkNzJob2d1OXBoM2VfMjAyMDA1MTlUMTQwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
           description:
             "Have you wondered how you can ask Google to show you the list of the top ten must-see places in Berlin? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.",
           visibility: "public",
-          venue: {
-            name: "Google",
-            address_1: "1 Lovely Road",
-            city: "Berlin",
-            localized_country_name: "Germany",
-          },
+          location: "Berlin, Germany",
           group: "Learn JavaScript - Berlin",
         }}
       />
@@ -42,15 +40,11 @@ describe("<Event /> component", () => {
     expect(EventWrapper.find(".Event")).toHaveLength(1);
     expect(EventWrapper.find(".time")).toHaveLength(1);
     expect(EventWrapper.find(".name")).toHaveLength(1);
-    expect(EventWrapper.find(".going")).toHaveLength(1);
   });
 
   test("render correct information", () => {
-    expect(EventWrapper.find(".time").text()).toEqual("16:00 - 2020-05-19");
+    expect(EventWrapper.find(".time").text()).toEqual("2020-05-19T16:00:00+02:00 - 2020-05-19T17:00:00+02:00");
     expect(EventWrapper.find(".name").text()).toEqual("Learn JavaScript");
-    expect(EventWrapper.find(".going").text()).toEqual(
-      "90 people who are going"
-    );
   });
 
   test("click on details button should expand event details", () => {
@@ -64,11 +58,6 @@ describe("<Event /> component", () => {
     expect(EventWrapper.find(".extra")).toHaveLength(0);
   });
 
-  test("click on details button should expand event details", () => {
-    EventWrapper.find(".details-btn").simulate("click");
-    expect(EventWrapper.find(".extra")).toHaveLength(1);
-  });
-
   test("click on details button should collapse event details", () => {
     EventWrapper.setState({ showDetails: true });
     EventWrapper.find(".details-btn").simulate("click");
@@ -77,7 +66,7 @@ describe("<Event /> component", () => {
 
   test("Display extra info", () => {
     EventWrapper.setState({ showDetails: true });
-    expect(EventWrapper.find(".extra .address")).toHaveLength(1);
+    expect(EventWrapper.find(".extra .location")).toHaveLength(1);
     expect(EventWrapper.find(".extra .visibility")).toHaveLength(1);
     expect(EventWrapper.find(".extra .link")).toHaveLength(1);
     expect(EventWrapper.find(".extra .description")).toHaveLength(1);
@@ -85,8 +74,8 @@ describe("<Event /> component", () => {
 
   test("Display correct extra info", () => {
     EventWrapper.setState({ showDetails: true });
-    expect(EventWrapper.find(".extra .address").text()).toEqual(
-      "Google, 1 Lovely Road, Berlin, Germany"
+    expect(EventWrapper.find(".extra .location").text()).toEqual(
+      "Berlin, Germany"
     );
     expect(EventWrapper.find(".extra .visibility").text()).toEqual("public");
     expect(EventWrapper.find(".extra .link").prop("href")).toEqual(
