105f04ec0a2857348ad7de8a42da0f0466517bf8
Jessica May <mayjessican@gmail.com>
2020-10-31 17:09:02 +0100

    Updates

--- precache-manifest.d521082b91a7c5f2600a1d5f9a357896.js
+++ precache-manifest.d521082b91a7c5f2600a1d5f9a357896.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c40327ca0144bd5bf3fd520b9f01e236",
+    "revision": "59cb3b0748f81a92f2eb9e95ea90bb0a",
     "url": "/meet/index.html"
   },
   {
-    "revision": "5c8491e4cc27ed9f2417",
+    "revision": "a9599c7985e4a9c82658",
     "url": "/meet/static/css/main.9f4697c2.chunk.css"
   },
   {
-    "revision": "72a102688c8d85c98782",
-    "url": "/meet/static/js/2.fdf8c3e9.chunk.js"
+    "revision": "fa70629e89c99d9128e3",
+    "url": "/meet/static/js/2.ea58b069.chunk.js"
   },
   {
     "revision": "93f865419f885acbc660c1dd85a44b33",
-    "url": "/meet/static/js/2.fdf8c3e9.chunk.js.LICENSE.txt"
+    "url": "/meet/static/js/2.ea58b069.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5c8491e4cc27ed9f2417",
-    "url": "/meet/static/js/main.c03c4bc5.chunk.js"
+    "revision": "a9599c7985e4a9c82658",
+    "url": "/meet/static/js/main.bc529105.chunk.js"
   },
   {
     "revision": "9de4f142a6170bcd9b6d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meet/precache-manifest.8aaa9ceb4ad0738dc4f0ab956ef0640b.js"
+  "/meet/precache-manifest.d521082b91a7c5f2600a1d5f9a357896.js"
 );
 
 self.addEventListener('message', (event) => {
